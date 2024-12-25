4ae0dd33b3b12d2cf3058a32329643d618ad32fa
Tejas Shah <tejasshah2k19@gmail.com>
2024-12-25 17:26:00 +0530

    get parameter - data read

--- src/main/java/com/controller/SignupServlet.java
+++ src/main/java/com/controller/SignupServlet.java
@@ -15,5 +15,36 @@ public class SignupServlet extends HttpServlet {
 			throws ServletException, IOException {
 
 		System.out.println("SignupServlet::service()");
+		// request -> process ->
+		// signup=> request -> process ->
+
+		// read data
+		// validation
+		// database save
+
+		// validation fail -> Signup
+		// true -> Login
+
+		// form->input->servlet->read?
+		String firstName = request.getParameter("firstName");// String
+		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		int bornYear = Integer.parseInt(request.getParameter("bornYear"));
+		String bornYear2 = request.getParameter("bornYear");
+
+//		age-> 34 -> String -> convert  
+		// encrypt
+		// plaintext -> enc
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(email);
+		System.out.println(password);
+		System.out.println(bornYear);
+		System.out.println(bornYear2);
+		// lib->
+
+		//
+
 	}
 }
